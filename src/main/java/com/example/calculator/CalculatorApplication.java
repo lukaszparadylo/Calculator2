701dda3627ed88d5aa0453f9274490e0fdb4aae5
lparadylo <lukaszparadylo@gmail.com>
2021-03-11 16:27:13 +0100

    Adding code to main class

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -1,5 +1,5 @@
 package com.example.calculator;
-
+import java.math.BigDecimal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +8,10 @@ public class CalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addTwoNumbers(new BigDecimal("3.3"),new BigDecimal("4.5")));
+        System.out.println(calculator.substractTwoNumbers(new BigDecimal("3.3"),new BigDecimal("4.5")));
     }
+
 
 }
